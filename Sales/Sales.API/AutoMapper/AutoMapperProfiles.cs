b89c658bb664468756cfa93117eb5f43d772ad34
mcastroh <m_castrohinostroza@hotmail.com>
2025-07-12 17:14:28 -0500

    Crea proyecto Sales

---
+++ Sales/Sales.API/AutoMapper/AutoMapperProfiles.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Sales.Shared.Entites;
+
+namespace Logistics.BackEnd.AutoMapper;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        #region Tablas => Genéricas
+
+        // <------ Almacén ------>
+        CreateMap<Country, BasicTablesDTO>().ReverseMap();
+        CreateMap<BasicTablesCreateDTO, Country>().ReverseMap();
+
+        //// <------ Grupo de Productos ------>
+        //CreateMap<Grupo, BasicTablesDTO>().ReverseMap();
+        //CreateMap<BasicTablesCreateDTO, Grupo>().ReverseMap();
+
+        //// <------ Tipos de Almacén ------>
+        //CreateMap<TipoAlmacen, BasicTablesDTO>().ReverseMap();
+        //CreateMap<BasicTablesCreateDTO, TipoAlmacen>().ReverseMap();
+
+        //// <------ Tipos de Artículos ------>
+        //CreateMap<TipoArticulo, BasicTablesDTO>().ReverseMap();
+        //CreateMap<BasicTablesCreateDTO, TipoArticulo>().ReverseMap();
+
+        //// <------ Sunat Tipo Existencias ------>
+        //CreateMap<SunatTipoExistencia, BasicTablesDTO>().ReverseMap();
+        //CreateMap<BasicTablesCreateDTO, SunatTipoExistencia>().ReverseMap();
+
+        //#endregion Tablas => Genéricas
+
+        //#region Tablas => Especializadas
+
+        //// <------ Parámetro de Configuración ------>
+        //CreateMap<ParametroConfiguracion, ParametroConfiguracionDTO>().ReverseMap();
+        //CreateMap<ParametroConfiguracionCreateDTO, ParametroConfiguracion>().ReverseMap();
+
+        //// <------ SubGrupo de Productos ------>
+        //CreateMap<SubGrupo, SubGrupoDTO>().ReverseMap();
+        //CreateMap<SubGrupoCreateDTO, SubGrupo>().ReverseMap();
+
+        //// <------ SubGrupo Detalles de Productos ------>
+        //CreateMap<SubGrupoDetalle, SubGrupoDetalleDTO>().ReverseMap();
+        //CreateMap<SubGrupoDetalleCreateDTO, SubGrupoDetalle>().ReverseMap();
+
+        //// <------ Sunat Detracciones ------>
+        //CreateMap<SunatDetraccion, SunatDetraccionDTO>().ReverseMap();
+        //CreateMap<SunatDetraccionCreateDTO, SunatDetraccion>().ReverseMap();
+
+        //
+        //
+        // <------ Editoriales ------>
+        //CreateMap<Editorial, EditorialesDTO>().ReverseMap();
+        //CreateMap<Editorial, EditorialesCreateDTO>().ReverseMap();
+
+        //// <------ Libros ------>
+        //CreateMap<Libro, LibrosDTO>().ReverseMap();
+        //CreateMap<Libro, LibrosCreateDTO>().ReverseMap();
+
+        //// <------ Autores ------>
+        //CreateMap<Autor, AutoresDTO>().ReverseMap();
+        //CreateMap<AutoresCreateDTO, Autor>().ForMember(m => m.Foto, options => options.Ignore());
+
+        #endregion Tablas => Genéricas
+    }
+}
---
+++ Sales/Sales.API/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sales Backend", Version = "v1" });
+});
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=CnSqlServer"));
+builder.Services.AddTransient<SeedDb>();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
