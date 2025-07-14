63b00cb977e379a5287b4a521363c9390d69e6f6
mcastroh <m_castrohinostroza@hotmail.com>
2025-07-14 11:38:49 -0500

    add-migration cityIndex

--- Sales/Sales.API/Data/DataContext.cs
+++ Sales/Sales.API/Data/DataContext.cs
@@ -9,19 +9,19 @@ public class DataContext : DbContext
     {
     }
 
+    public DbSet<Categoria> Categorias { get; set; }
     public DbSet<City> Cities { get; set; }
     public DbSet<Country> Countries { get; set; }
-    public DbSet<Categoria> Categorias { get; set; }
     public DbSet<State> States { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<City>().HasIndex(c => c.Name).IsUnique();
-        modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
         modelBuilder.Entity<Categoria>().HasIndex(c => c.Name).IsUnique();
-        modelBuilder.Entity<State>().HasIndex(c => c.Name).IsUnique();
+        modelBuilder.Entity<City>().HasIndex("Name", "StateId").IsUnique();
+        modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        modelBuilder.Entity<State>().HasIndex("Name", "CountryId").IsUnique();
     }
 
     /// <summary>
---
+++ Sales/Sales.API/Migrations/20250714163809_cityIndex.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sales.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class cityIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_States_Name",
+                table: "States");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_Name",
+                table: "Cities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_States_Name_CountryId",
+                table: "States",
+                columns: new[] { "Name", "CountryId" },
+                unique: true,
+                filter: "[CountryId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_Name_StateId",
+                table: "Cities",
+                columns: new[] { "Name", "StateId" },
+                unique: true,
+                filter: "[StateId] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_States_Name_CountryId",
+                table: "States");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cities_Name_StateId",
+                table: "Cities");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_States_Name",
+                table: "States",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_Name",
+                table: "Cities",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
